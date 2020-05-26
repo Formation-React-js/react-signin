561a0b862ca7463852eca7770739ff8032e8fdfd
Romain Tello <romain.tello@gmail.com>
2020-05-26 19:51:21 +0200

    Add login

---
+++ src/components/login-form.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { fakeLogin } from '../utils';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  return (
+    <Form
+      onSubmit={ (event) => {
+        event.preventDefault();
+        setErrorMessage('')
+        fakeLogin(username, password)
+        .then(response => console.log(response))
+        .catch(error => setErrorMessage("Identifiants invalides!"));
+      }}
+    >
+      {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
+      <Form.Group>
+        <Form.Label>Nom d'utilisateur</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Mot de passe</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </Form.Group>
+      <Button type="submit">
+        Se connecter
+      </Button>
+    </Form>
+  );
+}
+
+export default LoginForm;
---
+++ src/utils/fake-login.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    username: 'admin',
+    password: 'admin',
+    email: 'admin@test.com',
+    name: 'Jean-Pierre Admin',
+  },
+  {
+    username: 'moderator',
+    password: 'moderator',
+    email: 'moderator@test.com',
+    name: 'Jean-Michel Modo',
+  },
+  {
+    username: 'user',
+    password: 'user',
+    email: 'user@test.com',
+    name: 'Jean-Marie Youzeur',
+  },
+]
+
+export default (username, password) => new Promise( (resolve, reject) => {
+  for (const user of users) {
+    if (user.username === username && user.password === password) {
+      resolve(user);
+    }
+  }
+  reject();
+})
